8b84a4b6dec8b756108ad804b4a2edd10d74aee8
Digital Sleuth <62841822+digitalsleuth@users.noreply.github.com>
2021-01-13 14:02:06 +0000

    Add files via upload

---
+++ scripts/scripts/cleanup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+# Remove VMWare Tools
+rm -rf /home/mat/linux.iso
+
+# Remove Fonts that require License Agreement
+apt-get remove -y ttf-mscorefonts-installer
+
+# Make sure everything is up to date.
+apt-get update
+
+# Zero out the rest of the free space using dd, then delete the written file.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
---
+++ scripts/scripts/saltstack.sh
@@ -0,0 +1,18 @@
+echo "==> Installing salt-common"
+
+if [ "$DISTRO" == "bionic" ]; then
+  number="18.04"
+elif [ "$DISTRO" == "focal" ]; then
+  number="20.04"
+fi
+
+apt-get install -y software-properties-common
+wget -O - https://repo.saltstack.com/py3/ubuntu/$number/amd64/3001/SALTSTACK-GPG-KEY.pub | apt-key add -
+echo "deb [arch=amd64] http://repo.saltstack.com/py3/ubuntu/$number/amd64/3001 $DISTRO main" > /etc/apt/sources.list.d/saltstack.list
+apt-get update
+apt-get install -y salt-common
+
+echo "==> Basic salt-minion configuration"
+echo "mat_version: ${MAT_VERSION}" > /etc/salt/grains
+
+sleep 60
